200fa8ca544fcb7813309a4315726ca7258635cd
abc5259 <dlwogns3413@naver.com>
2020-04-30 03:08:26 +0900

    finish step2

--- localsMiddleware.js
+++ localsMiddleware.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube"; //res.locals.변수이름 = 변수값
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated : true,
+        id : 1,
+    }
     next()
 }
